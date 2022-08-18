d5521a8b5c18a34deb5110cbe4f6d52d6e28e62f
obedsonu <111181625+obedsonu@users.noreply.github.com>
2022-08-18 11:33:41 +0530

    Add files via upload

---
+++ hypotenuse calc practice program .js
@@ -0,0 +1,104 @@
+//console.log("welcome")
+//console.log(2*8)
+//let user="obed"
+//console.log(user)
+//let num1=7
+//let num2=3     
+//let user ="obed"
+//console.log(num1/num2, typeof user)
+//let num3= Number("333")
+//console.log(num3,typeof num3)
+//let num5=1
+//let num6=2
+//let result= num5+num6
+//console.log(result)
+//result= num5===num6 // check value and type
+//console.log(`the first number is ${num5} and second number is ${num6} total=${result}`)
+/*let person = {
+    firstName: `Obedullah`,
+    lastName: `Farooquei`,
+    age:`34`,
+    sayName() {
+        return `Hi my name is ${this.firstName} ${this.lastName} and age is ${this.age} `;
+    }
+};
+
+//console.log(person.sayName());*/
+
+// let num7=4
+// if (num7%2 === 0)
+//     console.log("number is even")
+// else
+//     console.log("number is odd")
+
+// let x = cal(4,3,3 );   // Function is called, return value will end up in x
+
+// function cal(a, b,_i )
+//  {
+// if (_i===1)
+//    { return a + b;  }           // Function returns the product of a and b
+//     else(_i===2)
+//     {return a-b;}
+
+// }
+// // console.log(`The result is ${x}`)
+// var == "saeed"
+// switch (var)
+// {
+//    case "afshin", "saeed", "larry":
+//        alert('Hey');
+//        break;
+
+//    default:
+//        alert('Default case');
+//        break;
+// }
+// let num1=12
+// let num1=1
+// let num2=2
+// let num3=3
+// let result
+// let result1
+
+// function add(n1,n2,n3)
+// {return n1+n2+n3;}
+// result =add(num1,num2,num3)
+
+
+// // // console.log(result)
+// for (let i=1;i<100;i++)
+// {
+//     if (i%3==0 && i>50 )// display div/3 and 50>
+//     console.log(i)
+// // }
+// let obed ={
+//     name:'Obedullah',
+//     surname:'Farooquei',
+//     age:34,
+//     kids:2
+// }
+// console.log(`The client name is ${obed.name} 
+// and surname is ${obed.surname} and age is ${obed.age}`)
+// // 
+// function student(name,age,rollno,sex){
+//     this.name=name;
+//     this.age=age;
+//     this.sex=sex;
+//     this.rollno=rollno;
+    
+// }
+// let obed = new student("Obedullah",29,30,"male")
+// console.log(obed);
+// let username = window.prompt("WHAT IS YOUR NAAM")
+// console.log(username)
+// let age = window.prompt("WHAT IS YOUR AGE")
+// console.log(typeof age)
+// age= Number(age)
+// console.log(age)
+// console.log(typeof age);
+let a,b,c;
+//hypotenuse calc practice program 
+a = Number(window.prompt("enter side A"));
+b = Number( window.prompt("enter side B"));
+c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2))
+console.log("the hypotenuse lenght is "+c);
