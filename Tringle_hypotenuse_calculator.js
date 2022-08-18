c3d4f355a5ec8ab711f360060db11a73edb450a7
obedsonu <111181625+obedsonu@users.noreply.github.com>
2022-08-18 14:09:18 +0530

    Add files via upload

---
+++ Tringle_hypotenuse_calculator.js
@@ -0,0 +1,6 @@
+let a,b,c;
+//hypotenuse calc practice program 
+a = Number(window.prompt("enter side A"));
+b = Number( window.prompt("enter side B"));
+c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2))
+console.log("the hypotenuse lenght is "+c);
